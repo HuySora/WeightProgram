9f787e41aa5b6249700dad57cf0867cc487c4f59
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2024-12-27 17:37:54 +0700

    + MainForm: Renamed from default project Form1

---
+++ MainForm.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+namespace WeightProgram {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
+    }
+}
