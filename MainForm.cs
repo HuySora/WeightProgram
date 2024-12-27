94a55f1c21bc8abd87f199ab451048c6d9beedcf
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2024-12-28 04:57:41 +0700

    + MainForm: Fix resize lag

--- MainForm.cs
+++ MainForm.cs
@@ -4,6 +4,8 @@ namespace WeightProgram {
     public partial class MainForm : Form {
         public MainForm() {
             InitializeComponent();
+            ResizeBegin += (s, e) => { SuspendLayout(); };
+            ResizeEnd += (s, e) => { ResumeLayout(true); };
         }
     }
 }
