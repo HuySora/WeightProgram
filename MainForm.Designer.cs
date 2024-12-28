a90c71a55c77822501a9dd7973ef66587fb97535
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2024-12-28 07:28:14 +0700

    Update MainForm to be a MDI container, also add a toolbar menu to open WeightInputForm

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -23,13 +23,52 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1124, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(112, 19);
+            this.toolStripMenuItem1.Text = "Chương trình cân";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1124, 602);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -10,10 +10,30 @@ using System.Windows.Forms;
 
 namespace WeightProgram {
     public partial class MainForm : Form {
+        private WeightInputForm m_WeightInputForm;
+        private string m_WeightInputFormName = "WeightInputForm";
         public MainForm() {
             InitializeComponent();
             ResizeBegin += (s, e) => { SuspendLayout(); };
             ResizeEnd += (s, e) => { ResumeLayout(true); };
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e) {
+            if(m_WeightInputForm == null) {
+                m_WeightInputForm = Application.OpenForms[m_WeightInputFormName] as WeightInputForm;
+                if(m_WeightInputForm == null) {
+                    m_WeightInputForm = new WeightInputForm();
+                    // TODO Performance: Garbage collection
+                    m_WeightInputForm.FormClosed += (x, y) => m_WeightInputForm = null;
+                }
+                m_WeightInputForm.MdiParent = this;
+            }
+            // QoL: Remember last window state instead of default to Normal
+            if(m_WeightInputForm.WindowState == FormWindowState.Minimized) {
+                m_WeightInputForm.WindowState = FormWindowState.Normal;
+            }
+            m_WeightInputForm.BringToFront();
+            m_WeightInputForm.Show();
         }
     }
 }
