8dc9fac5a782e8ec4055b8ba9c5a1f98b53cfd17
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2025-01-21 00:27:13 +0700

    + MainForm: Add a toolbar menu to open WeightInputForm

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -23,21 +23,52 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.btnOpenWeightInputForm = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnOpenWeightInputForm});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip.Size = new System.Drawing.Size(1124, 25);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // btnOpenWeightInputForm
+            // 
+            this.btnOpenWeightInputForm.Name = "btnOpenWeightInputForm";
+            this.btnOpenWeightInputForm.Size = new System.Drawing.Size(112, 19);
+            this.btnOpenWeightInputForm.Text = "Chương trình cân";
+            this.btnOpenWeightInputForm.Click += new System.EventHandler(this.btnOpenWeightInputForm_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1124, 602);
+            this.Controls.Add(this.menuStrip);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip;
             this.Name = "MainForm";
             this.Text = "MainForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem btnOpenWeightInputForm;
     }
 }
 
