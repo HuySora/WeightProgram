5b66b65ce379574e8de977641118138845c720ca
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2025-01-21 00:28:36 +0700

    + FormExtension.cs: Extension for WinForm related class

---
+++ Util/FormExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WeightProgram.Util {
+    public static class FormExtension {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="textBox"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool TryParseString(this TextBox textBox, out string value) {
+            value = string.Empty;
+            if(string.IsNullOrWhiteSpace(textBox.Text)) {
+                return false;
+            }
+            value = textBox.Text;
+            return true;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="textBox"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool TryParseInt(this TextBox textBox, out int value) {
+            value = 0; // Default value
+            if(string.IsNullOrWhiteSpace(textBox.Text)) {
+                return false;
+            }
+            string inputTxt = textBox.Text.Replace(",", "").Trim();
+            if(!int.TryParse(inputTxt, out value)) {
+                return false;
+            }
+            return true;
+        }
+    }
+}
