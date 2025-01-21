a31eb6823d7837c52025008d8adfc3fbbb10aba4
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2025-01-22 02:45:45 +0700

    + WeightDataReceiptReportViewerForm

---
+++ WeightDataReceiptReportViewerForm.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace WeightProgram {
+    partial class WeightDataReceiptReportViewerForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // reportViewer
+            // 
+            this.reportViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewer.LocalReport.ReportEmbeddedResource = "WeightProgram.Reports.WeightDataReceipt.rdlc";
+            this.reportViewer.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer.Name = "reportViewer";
+            this.reportViewer.ServerReport.BearerToken = null;
+            this.reportViewer.Size = new System.Drawing.Size(1108, 563);
+            this.reportViewer.TabIndex = 0;
+            // 
+            // WeightDataReceiptReportViewerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1108, 563);
+            this.Controls.Add(this.reportViewer);
+            this.Name = "WeightDataReceiptReportViewerForm";
+            this.Text = "WeightDataReceiptReportViewerForm";
+            this.Load += new System.EventHandler(this.WeightDataReceiptReportViewerForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
+    }
+}
---
+++ WeightDataReceiptReportViewerForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WeightProgram {
+    public partial class WeightDataReceiptReportViewerForm : Form {
+        public WeightDataReceiptReportViewerForm() {
+            InitializeComponent();
+        }
+
+        private void WeightDataReceiptReportViewerForm_Load(object sender, EventArgs e) {
+
+            this.reportViewer.RefreshReport();
+        }
+    }
+}
