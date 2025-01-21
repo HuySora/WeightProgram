08280c569ac8ebe2b069ed5fea2f0f54d17907e9
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2025-01-22 02:46:22 +0700

    + PrintExtension.cs

---
+++ Util/PrintExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing.Printing;
+
+namespace WeightProgram.Util {
+    public static class PrintExtension {
+        public static bool TryGetPaperSize(this PrinterSettings printerSettings, PaperKind paperKind, out PaperSize paperSize) {
+            paperSize = null;
+            PrinterSettings settings = new PrinterSettings();
+            foreach(PaperSize size in settings.PaperSizes) {
+                if(size.Kind == paperKind) {
+                    paperSize = size;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
