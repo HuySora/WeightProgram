4e8ed3447888ccca18a28f6ca3df78141cd872cd
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2025-01-21 20:10:00 +0700

    + MainForm: Add SerialPort connection and it status to status strip

--- MainForm.cs
+++ MainForm.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Data.Common;
 using System.Drawing;
+using System.IO.Ports;
+using System.Text;
 using System.Windows.Forms;
 using WeightProgram.Data;
 
@@ -12,7 +14,12 @@ namespace WeightProgram {
         public WeightProgramModelContainer DbContext { get; private set; }
         public WeightDataService WeightDataService { get; private set; }
 
+        public int CurrentScaleWeight { get; private set; }
+        private SerialPort m_SerialPort;
+        private StringBuilder m_InputBuffer;
+
         private ToolStripStatusLabel lbl_DatabaseStatus;
+        private ToolStripStatusLabel lbl_SerialPortStatus;
 
         public Timer UpdateTimer { get; private set; }
 
@@ -28,10 +35,17 @@ namespace WeightProgram {
             DbContext = new WeightProgramModelContainer();
             WeightDataService = new WeightDataService(DbContext);
             DbContext.WeightDatum.Find(0); // Initialize database connection
+            // SerialPort
+            m_SerialPort = new SerialPort("COM1", 1200, Parity.Even, 7, StopBits.One);
+            m_SerialPort.Encoding = Encoding.ASCII;
+            m_SerialPort.DataReceived += SerialPort_DataReceived;
             // Status strip
             lbl_DatabaseStatus = new ToolStripStatusLabel();
             lbl_DatabaseStatus.Alignment = ToolStripItemAlignment.Left;
             statusStrip.Items.Add(lbl_DatabaseStatus);
+            lbl_SerialPortStatus = new ToolStripStatusLabel();
+            lbl_SerialPortStatus.Alignment = ToolStripItemAlignment.Left;
+            statusStrip.Items.Add(lbl_SerialPortStatus);
             // Timer
             UpdateTimer = new Timer();
             UpdateTimer.Interval = 1000;
@@ -83,6 +97,47 @@ namespace WeightProgram {
                 }
             } catch(DbException ex) {
                 Console.WriteLine(ex.Message);
+            }
+
+            switch(m_SerialPort.IsOpen) {
+                case true:
+                    lbl_SerialPortStatus.ForeColor = Color.Green;
+                    break;
+                case false:
+                    lbl_SerialPortStatus.ForeColor = Color.Red;
+                    break;
+            }
+            // Try to reconnect SerialPort if not connected
+            lbl_SerialPortStatus.Text = $"SerialPort={(m_SerialPort.IsOpen ? "Open" : "Closed")}";
+            // TODO: Is there anyway to get serial port connection state string?
+            try {
+                if(!m_SerialPort.IsOpen) {
+                    m_SerialPort.Open();
+                }
+            } catch(Exception ex) {
+                lbl_SerialPortStatus.Text = $"SerialPort={ex.Message}";
+            }
+        }
+        #endregion
+
+        #region Other event
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e) {
+            // Read the incoming data
+            string incomingData = ((SerialPort)sender).ReadExisting();
+            m_InputBuffer.Append(incomingData);
+            // Cleanup unrelated data
+            while(m_InputBuffer.Length > 1 && m_InputBuffer[0] != (char)2) {
+                // Remove the first character
+                m_InputBuffer.Remove(0, 1);
+            }
+            if(m_InputBuffer.Length >= 10) {
+                // Get substring from index 4 to 9
+                string strDisp = m_InputBuffer.ToString().Substring(4, 5);
+                // Remove the first 15 characters
+                m_InputBuffer.Remove(0, Math.Min(m_InputBuffer.Length, 15));
+                if(int.TryParse(strDisp, out int weight)) {
+                    CurrentScaleWeight = weight;
+                }
             }
         }
         #endregion
