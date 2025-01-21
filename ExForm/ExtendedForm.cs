18782c1f38135b799a541d4f9c9ca0b58eb802e8
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2025-01-21 14:21:00 +0700

    + ExtendedForm.cs: Extended version of Form for extra functionality

---
+++ ExForm/ExtendedForm.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Forms;
+
+namespace WeightProgram.ExForm {
+    public class ExtendedForm<TMdiForm> : Form where TMdiForm : Form {
+        public new TMdiForm MdiParent {
+            get => (TMdiForm)base.MdiParent;
+            set => base.MdiParent = value;
+        }
+    }
+}
