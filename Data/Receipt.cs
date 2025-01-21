f8f5f9b9949b5b85860bbd3f7fe52675eea825aa
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2025-01-22 04:20:02 +0700

    + Receipt.cs: Add 'GetReceiptCodeShort' property to get shortened version of ReceiptCode

--- Data/Receipt.cs
+++ Data/Receipt.cs
@@ -4,6 +4,14 @@ using System;
 
 namespace WeightProgram.Data {
     public partial class Receipt {
+        public string GetReceiptCodeShort {
+            get {
+                // Get the last maximum 7 characters
+                int startIndex = Math.Max(0, ReceiptCode.Length - 7);
+                int length = Math.Min(ReceiptCode.Length, 7);
+                return ReceiptCode.Substring(startIndex, length);
+            }
+        }
         public void Evaluate() {
             var encoder = new ScryptEncoder();
             ReceiptCode = encoder.Encode($"{Id}").ToBase62();
