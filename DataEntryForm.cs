305d479a7a4a3c43af517c920d5dc1439baf0545
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2025-01-24 18:10:19 +0700

    + DataEntryForm: Fix resize lag

--- DataEntryForm.cs
+++ DataEntryForm.cs
@@ -4,6 +4,8 @@ namespace WeightProgram {
     public partial class DataEntryForm : Form {
         public DataEntryForm() {
             InitializeComponent();
+            ResizeBegin += (s, e) => { SuspendLayout(); };
+            ResizeEnd += (s, e) => { ResumeLayout(true); };
         }
     }
 }
